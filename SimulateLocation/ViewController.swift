abdfb460c56b3668cbbc737357b42ef0d6b91f8c
mangonob <mangonob@qq.com>
2023-09-15 16:57:59 +0800

    feat: initial

--- SimulateLocation/ViewController.swift
+++ SimulateLocation/ViewController.swift
@@ -6,14 +6,49 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var latLabel: UILabel!
+    @IBOutlet weak var lonLabel: UILabel!
+    
+    let defaultRegion = MKCoordinateRegion(center: .init(latitude: 22.547990410719006, longitude: 113.94235976008504), span: .init(latitudeDelta: 0.004, longitudeDelta: 0.004))
+    let meilinRegion = MKCoordinateRegion(center: .init(latitude: 22.573216080521416, longitude: 114.0608951326087), span: .init(latitudeDelta: 0.004, longitudeDelta: 0.004))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        mapView.delegate = self
+        mapView.setRegion(defaultRegion, animated: true)
     }
+    
+    @IBAction func currentLocationAction(_ sender: Any) {
+        mapView.setRegion(defaultRegion, animated: true)
+    }
+    
+    @IBAction func toMeilinAction(_ sender: Any) {
+        mapView.setRegion(meilinRegion, animated: true)
+    }
+    
+    @IBAction func copyLatitude(_ sender: Any) {
+        UIPasteboard.general.string = "\(mapView.region.center.latitude)"
+    }
+    
+    @IBAction func copyLongitude(_ sender: Any) {
+        UIPasteboard.general.string = "\(mapView.region.center.longitude)"
+    }
+    
+    fileprivate func updateLabel(location: CLLocationCoordinate2D) {
+        latLabel.text = "Lat: \(location.latitude)"
+        lonLabel.text = "Lat: \(location.longitude)"
+    }
+}
 
-
+extension ViewController: MKMapViewDelegate {
+    func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
+        updateLabel(location: mapView.region.center)
+    }
 }
 
